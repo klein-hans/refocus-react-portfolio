6042fb3ee92e28673d70cfe9ed50dac38f70d6d8
Klein <escutonklein@gmail.com>
2023-04-11 19:55:42 +0800

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import {
+    Route,
+    createBrowserRouter,
+    createRoutesFromElements,
+    RouterProvider,
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import About from './pages/About';
+import Contact, { contactAction } from './pages/Contact';
+import NotFound from './pages/NotFound';
+import RootLayout from './layouts/RootLayout';
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<RootLayout />}>
+            <Route index element={<Home />} />
+            <Route path='about' element={<About />} />
+            <Route
+                path='contact'
+                element={<Contact />}
+                action={contactAction}
+            />
+            <Route path='services' element={<Services />} />
+            <Route path='*' element={<NotFound />} />
+        </Route>
+    )
+);
 
 function App() {
-    const [count, setCount] = useState(0);
-
-    return (
-        <div className='App'>
-            <div></div>
-        </div>
-    );
+    return <RouterProvider router={router} />;
 }
 
 export default App;
